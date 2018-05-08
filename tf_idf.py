260c1cec60f632f1d7071f82bf52f0416187ada7
Agulaa <agnieszkaxrusin@gmail.com>
2018-05-08 11:01:32 +0200

    update

--- tf_idf.py
+++ tf_idf.py
@@ -1,20 +1,18 @@
 from nltk.corpus import reuters
 from nltk.stem.porter import *
 from nltk.corpus import stopwords
-from nltk.stem.lancaster import LancasterStemmer
 
 
 
 __dic = dict()
 __re_dic = {}
 __freq = {}
-__all_files = {}
-__word_vector_file = {}
-__binary_vector_file = {}
+__word_freq_vector_file = {}
+__freq_vector_file = {}
+__all_freq = {}
 
 
-
-def preprocessig_stemming_and_lower_words(words):
+def preprocessing(words):
     #delete ending
     stemmer = PorterStemmer()
     singles = [stemmer.stem(word) for word in words]
@@ -30,60 +28,55 @@ def preprocessig_stemming_and_lower_words(words):
     clean_words = [word for word in words_without_stop if word.isalpha() or word.isdigit() or word.isalnum()]
     return  clean_words
 
-def create_dictionary(words, iterator):
+
+
+def create_global_dictionary_words(words, iterator):
+    #do global dictionary with all words in corups
     for word in words:
         if word not in __dic:
             __dic[word] = iterator
             __re_dic[iterator] = word
             iterator += 1
-            __freq[word] = 1
+
+def create_gloal_dictionary_freq_word_file(words, file):
+    all_freq = {}
+    for word in words:
+        if word not in all_freq:
+            all_freq[word] = 1
         else:
-            __freq[word] += 1
+            all_freq[word] += 1
+
+    __word_freq_vector_file[file] = all_freq
 
 
-def do_dict_for_all_file():
+
+def do_dicts_for_all_file():
+    #fill global dict with words and fill global dic freq and fill
     iterator = 0
     i = 0
     for category in reuters.categories():
         for file in reuters.fileids(category):
-            __all_files[i] = file
             words = reuters.words(file)
-            lower_single = preprocessig_stemming_and_lower_words(words)
-            all_word = []
-            for word in lower_single:
-                if word not in all_word:
-                    all_word.append(word)
-
-            __word_vector_file[file] = all_word
-            i += 1
-            create_dictionary(lower_single, iterator)
+            clean_words = preprocessing(words)
+            create_global_dictionary_words(clean_words, iterator)
+            create_gloal_dictionary_freq_word_file(clean_words,file)
 
 
-
-def create_document_word_matrix():
-
-    for file,words in __word_vector_file.items():
-        binary = []
-        for index, word in __re_dic.items():
-            if word not in words:
-                binary.append(0)
+def create_document_word_freq_matrix():
+    for file, words in __word_freq_vector_file.items():
+        vec = []
+        for index, w in __re_dic.items():
+            if w in words:
+                vec.append(words[w])
             else:
-                binary.append(1)
-        __binary_vector_file[file] = binary
+                vec.append(0)
+        __freq_vector_file[file] = vec
 
 
 
 if __name__ == '__main__':
-    do_dict_for_all_file()
-    i = 0
-    for k,v in __word_vector_file.items():
-        print(k,v)
-        if i == 5:
-            break
-        else: i+=1
-    #print('vector \n',__word_vector_file)
+    do_dicts_for_all_file()
+    create_document_word_freq_matrix()
 
-    # print('Dictionary \n',__dic)
-    # print('Re_dictionary \n', __re_dic)
-    # print('Frequency \n', __freq)
-    print('All files', len(__all_files))
+
+
